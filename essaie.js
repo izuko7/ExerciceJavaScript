2b2f02873c519f4ed1337c590902ecdb66dd0f1f
Campbell Miha <izuko7mha@gmail.com>
2026-03-12 15:40:00 +0000

    second commit

--- essaie.js
+++ essaie.js
@@ -1,56 +1,56 @@
 
-// let nombreCaractere = prompt("Combien de caractère voulez-vous ?")
+let nombreCaractere = prompt("Combien de caractère voulez-vous ?")
 
-// function genererCaracteres(longueur) {
-//   const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-/;,=:+ù%$*&é"(§è!çà)';
-//   let resultat = '';
-//   for (let i = 0; i < longueur; i++) {
-//     resultat += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
-//   }
-//   return resultat;
-// }
-// alert(genererCaracteres(nombreCaractere)); // Exemple: "aB3dE5fGhI"
-
-
+function genererCaracteres(longueur) {
+  const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-/;,=:+ù%$*&é"(§è!çà)';
+  let resultat = '';
+  for (let i = 0; i < longueur; i++) {
+    resultat += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
+  }
+  return resultat;
+}
+alert(genererCaracteres(nombreCaractere)); // Exemple: "aB3dE5fGhI"
 
 
 
-// let essai=0;
-// let continuerPartie = true;
-
-// function nombreAleatoire(min, max) {
-//   return Math.floor(Math.random() * (max - min + 1));
-// }
-
-// nombreAleatoire(1, 100); // nombre entre 1 et 10
-
-// let result = nombreAleatoire(1,100);
-// console.log(result);
-
-// let nbre = prompt("Enter un nombre"); 
-// nbre = parseInt(nbre);
 
 
-// while(continuerPartie){
-//     // essai++;
-//     if(nbre === result){
-//         alert("vous avez gagnez !");
-//         continuerPartie = false;
-//     }else if(nbre<result){
-//         essai++
-//         nbre = prompt("Entrer un nombre plus grand");
-//         nbre = parseInt(nbre);
-//     } else {
-//         essai++
-//         nbre = prompt("Entrer un nombre plus petit");
-//         nbre = parseInt(nbre);
-//     }
+let essai=0;
+let continuerPartie = true;
 
-//     if(essai>=8){
-//         alert("Ton papa, Tu as eu " + essai + " essaies");
-//         continuerPartie = false;
-//     }
-// }
+function nombreAleatoire(min, max) {
+  return Math.floor(Math.random() * (max - min + 1));
+}
+
+nombreAleatoire(1, 100); // nombre entre 1 et 10
+
+let result = nombreAleatoire(1,100);
+console.log(result);
+
+let nbre = prompt("Enter un nombre"); 
+nbre = parseInt(nbre);
+
+
+while(continuerPartie){
+    // essai++;
+    if(nbre === result){
+        alert("vous avez gagnez !");
+        continuerPartie = false;
+    }else if(nbre<result){
+        essai++
+        nbre = prompt("Entrer un nombre plus grand");
+        nbre = parseInt(nbre);
+    } else {
+        essai++
+        nbre = prompt("Entrer un nombre plus petit");
+        nbre = parseInt(nbre);
+    }
+
+    if(essai>=8){
+        alert("Ton papa, Tu as eu " + essai + " essaies");
+        continuerPartie = false;
+    }
+}
 
 
 
@@ -64,18 +64,16 @@
 
 
 
-// function timer(secondes) {
-//     if(secondes > 0){
-//         console.log(secondes);
-//         timer(secondes-1);
-//     } else {
-//         console.log(secondes);
-//     }
-// }
+function timer(secondes) {
+    if(secondes > 0){
+        console.log(secondes);
+        timer(secondes-1);
+    } else {
+        console.log(secondes);
+    }
+}
 
-// timer(10)
-
-
+timer(10)
 
 
 
@@ -83,131 +81,133 @@
 
 
 
-// if(age < 13){
-//     console.log(" Jte recommande le film LILO ET STICHT");
-// } else if(age >= 18) {
-//     console.log("Jte recommande le fiml EVIL DEAD");
-// } else {
-//     console.log("Jte recommande le film MATRIX");
-// }
 
-// try {
-//     alert(hello);
-// } catch (error){
-//     alert(error.stack);
-// }
 
-// let i = 0;
+if(age < 13){
+    console.log(" Jte recommande le film LILO ET STICHT");
+} else if(age >= 18) {
+    console.log("Jte recommande le fiml EVIL DEAD");
+} else {
+    console.log("Jte recommande le film MATRIX");
+}
 
-// while( i < 10 ) {
+try {
+    alert(hello);
+} catch (error){
+    alert(error.stack);
+}
 
-//     if(i == 8) {
-//         break;
-//     }
+let i = 0;
 
-//     console.log("ligne : " + i);
-//     i++;
-// }
+while( i < 10 ) {
 
-// let gareDeDepart = "Abidjan";
-// let gareDArriver = "";
-// let chauffeur = "Chauffaur1";
+    if(i == 8) {
+        break;
+    }
 
-// if ((gareDeDepart != "" || gareDArriver != "") && chauffeur != "") {
-//     console.log("Le train peut demarer");
-// } else {
-//     console.log("Le train ne peut pas demarer");
-// }
+    console.log("ligne : " + i);
+    i++;
+}
 
-// let x = 5;
+let gareDeDepart = "Abidjan";
+let gareDArriver = "";
+let chauffeur = "Chauffaur1";
 
-// (x > 3) ? console.log("") : (false);
+if ((gareDeDepart != "" || gareDArriver != "") && chauffeur != "") {
+    console.log("Le train peut demarer");
+} else {
+    console.log("Le train ne peut pas demarer");
+}
 
-// let superHero = prompt("Donner le nom d'un superhero, je vous dirai si il est de Marvel ou DC comics");
+let x = 5;
 
-// switch(superHero) {
+(x > 3) ? console.log("") : (false);
 
-//     case 'Iron Man':
-//     case 'Captain America':
-//     case 'Thor':
-//     case 'Hulk':
-//     case 'Black Panther':
-//     case 'Spider-Man':
-//     case 'Ant-Man':
-//     case 'Captain Marvel':
-//         alert("C'est un hero Marvel !");
-//         break;
+let superHero = prompt("Donner le nom d'un superhero, je vous dirai si il est de Marvel ou DC comics");
 
-//     case 'Batman':
-//     case 'Superman':
-//     case 'Wonder Woman':
-//     case 'Flash':
-//     case 'Aquaman':
-//     case 'Shazam':
-//     case 'Green Lantern':
-//         alert("C'est un hero DC Comics !");
-//         break;
+switch(superHero) {
 
-//     default:
-//         alert("Oups revoyer l'écriture !");
-// }
+    case 'Iron Man':
+    case 'Captain America':
+    case 'Thor':
+    case 'Hulk':
+    case 'Black Panther':
+    case 'Spider-Man':
+    case 'Ant-Man':
+    case 'Captain Marvel':
+        alert("C'est un hero Marvel !");
+        break;
 
-// function demanderAge (){
+    case 'Batman':
+    case 'Superman':
+    case 'Wonder Woman':
+    case 'Flash':
+    case 'Aquaman':
+    case 'Shazam':
+    case 'Green Lantern':
+        alert("C'est un hero DC Comics !");
+        break;
 
-//     let age = prompt("Quel est votre age ?");
-//     alert ("Votre age est : " + age + " ans.");
-//     console.log(age)
+    default:
+        alert("Oups revoyer l'écriture !");
+}
 
-// }
+function demanderAge (){
 
-// demanderAge();
+    let age = prompt("Quel est votre age ?");
+    alert ("Votre age est : " + age + " ans.");
+    console.log(age)
 
-//   let poids = prompt("Quel est votre poids en Kg?");
-//   let taille = prompt("Quelle est votre taille en m?");
-//   poids = parseFloat(poids);
-//   taille = parseFloat(taille);
-//   console.log(poids, taille);
+}
 
-//   function calculerIMC () {
+demanderAge();
 
-//   }
+  let poids = prompt("Quel est votre poids en Kg?");
+  let taille = prompt("Quelle est votre taille en m?");
+  poids = parseFloat(poids);
+  taille = parseFloat(taille);
+  console.log(poids, taille);
 
-// let poids = prompt("Quel est votre poids en Kg?");
-// let taille = prompt("Quelle est votre taille en m?");
+  function calculerIMC () {
 
-// poids = parseFloat(poids);
-// taille = parseFloat(taille);
+  }
 
-// console.log(poids, taille);
+let poids = prompt("Quel est votre poids en Kg?");
+let taille = prompt("Quelle est votre taille en m?");
 
-// function calculerIMC() {
-//     let resultatImc = poids/(taille**2);
-//     return resultatImc;
-// }
+poids = parseFloat(poids);
+taille = parseFloat(taille);
 
-// calculerIMC(poids, taille)
+console.log(poids, taille);
 
-// let afficherImc = calculerIMC(poids, taille);
+function calculerIMC() {
+    let resultatImc = poids/(taille**2);
+    return resultatImc;
+}
 
-// alert("Votre IMC est : " + afficherImc);
-// console.log(calculerIMC(poids, taille));
+calculerIMC(poids, taille)
 
-// let poids = prompt("Quel est votre poids en kg?");
-// let taille = prompt("Quelle est votre taille en mètre");
+let afficherImc = calculerIMC(poids, taille);
 
-// function calculerIMC(poids, taille) {
+alert("Votre IMC est : " + afficherImc);
+console.log(calculerIMC(poids, taille));
 
-//     let tailleCalculer = Math.pow(taille, 2);
-//     let resultatImc = poids / tailleCalculer;
+let poids = prompt("Quel est votre poids en kg?");
+let taille = prompt("Quelle est votre taille en mètre");
 
-//     return resultatImc;
+function calculerIMC(poids, taille) {
 
-// }
+    let tailleCalculer = Math.pow(taille, 2);
+    let resultatImc = poids / tailleCalculer;
 
-// let afficherImc = calculerIMC(poids, taille);
-// alert("Votre IMC est : " + afficherImc);
+    return resultatImc;
 
-// console.log(calculerIMC(poids, taille));
+}
+
+let afficherImc = calculerIMC(poids, taille);
+alert("Votre IMC est : " + afficherImc);
+
+console.log(calculerIMC(poids, taille));
 
 
 
--- index.js
+++
@@ -1,1 +0,0 @@
-console.log("slaut je suis u ntest")
